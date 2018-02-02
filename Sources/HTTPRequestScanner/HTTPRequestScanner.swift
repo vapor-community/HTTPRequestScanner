5899a92fd6e06b4008296f2cbf3e4f215378b747
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-02 17:04:15 -0600

    Created HTTPRequestScanner.onPath handler

--- Sources/HTTPRequestScanner/HTTPRequestScanner.swift
+++ Sources/HTTPRequestScanner/HTTPRequestScanner.swift
@@ -8,4 +8,7 @@ public struct HTTPRequestScanner {
     
     /// Called on a data chunk scanned from the request's status code.
     var onStatusCode: (UnsafeBufferPointer<UInt8>) -> Void = {_ in}
+    
+    /// Called on a data chunk scanned from the request's route path.
+    var onPath: (UnsafeBufferPointer<UInt8>) -> Void = {_ in}
 }
