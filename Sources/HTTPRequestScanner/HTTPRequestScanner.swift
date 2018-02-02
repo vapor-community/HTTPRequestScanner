5e2bbdf3ae39de57e700de2081a10ea3a969ada2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-02 16:59:33 -0600

    Created HTTPRequestScanner.onMethod handler

--- Sources/HTTPRequestScanner/HTTPRequestScanner.swift
+++ Sources/HTTPRequestScanner/HTTPRequestScanner.swift
@@ -1,1 +1,8 @@
+import Async
 
+/// Scans an incoming HTTP request as a `ByteParserStream`.
+public struct HTTPRequestScanner {
+    
+    /// Called on a data chunk scanned from the request's method.
+    var onMethod: (UnsafeBufferPointer<UInt8>, Int) -> Void = {_, _ in}
+}
