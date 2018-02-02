e0d4c3158a420a8bc5757bb95161dc3627275a4f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-02 17:01:36 -0600

    Created HTTPRequestScanner.onStatusCode handler

--- Sources/HTTPRequestScanner/HTTPRequestScanner.swift
+++ Sources/HTTPRequestScanner/HTTPRequestScanner.swift
@@ -5,4 +5,7 @@ public struct HTTPRequestScanner {
     
     /// Called on a data chunk scanned from the request's method.
     var onMethod: (UnsafeBufferPointer<UInt8>) -> Void = {_ in}
+    
+    /// Called on a data chunk scanned from the request's status code.
+    var onStatusCode: (UnsafeBufferPointer<UInt8>) -> Void = {_ in}
 }
